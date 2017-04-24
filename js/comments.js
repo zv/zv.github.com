200cff54f10ee1c18135f285bbb29415c9dc16b1
Zephyr Pellerin <zv@nxvr.org>
2017-04-24 00:53:02 -0700

    Replace dead comment loading code

--- js/comments.js
+++ js/comments.js
@@ -1,3 +1,5 @@
+"use strict";
+
 /*
  * This function loads a list of comments sourced from Github issues into a
  * blogpost ("page") by enumerating our github issues, finding which corresponds
@@ -22,49 +24,52 @@ function load_comments(github_id, repo_issues_url, issue_id) {
         </div>
         <div class="comment-right">
             <div class="comment-header">
-                <strong>${issue.user.login}</strong>
+
             </div>
             <hr />
             <div class="body"> ${issue.body_html} </div>
         </div>
-    </div>`
+    </div>`;
     }
 
-    function find_comments_url(callback) {
+    // Issues must be referenced by their numeric Github ID, `find_comments_url`
+    // resolves a local blogpost/page name to that numeric ID.
+    function find_comments_url() {
         // we append 'created' to ensure only we can create comment threads
-        fetch(`${repo_issues_url}?created=${github_id}`, {
-            headers:  {
-                "Accept": "application/vnd.github.v3.html+json"
-            }})
+        return window.fetch(`${repo_issues_url}?created=${github_id}`, {
+            headers: { "Accept": "application/vnd.github.v3.html+json" }})
             .then(response => response.json())
             .then(json => {
-                console.log(json);
-                var issues = json.filter(issue => issue.title === issue_id)
-                if (issues.length === 1)
-                    // now that we've found the URL of the page hosting our
-                    // current page's comments, let fetch them.
-                    callback(issues[0].html_url, issues[0].comments_url)
-            })
+                var issues = json.filter(issue => issue.title === issue_id);
+                if (issues.length === 1) return issues;
+                else throw new Error(`Incorrect number of issues: ${issues.length}`);
+            });
     }
 
-    function list_comments(html_issues_url, api_issues_url) {
-        var container = document.querySelector(COMMENT_CONTAINER);
-        var howto = document.querySelector(COMMENT_HOWTO);
-        fetch(api_issues_url, {
-            headers:  {
-                "Accept": "application/vnd.github.v3.html+json"
-            }})
+    // Fetch (and insert) our comments
+    function fetch_comments(html_issues_url, api_issues_url) {
+        return window.fetch(api_issues_url, {
+            headers: { "Accept": "application/vnd.github.v3.html+json" }})
             .then(response => response.json())
-            .then(comments => {
-                comments.forEach(comment => {
-                    var elt = document.createElement('div');
-                    elt.innerHTML = build_comment_html(comment);
-                    container.appendChild(elt);
-                })
-            })
-        container.style.display = 'block';
-        howto.href = html_issues_url;
+            .then(comments => [comments, html_issues_url]);
     }
 
-    find_comments_url(list_comments);
+    function insert_comments(comments, issues_url) {
+        var howto = document.querySelector(COMMENT_HOWTO);
+        var container = document.querySelector(COMMENT_CONTAINER);
+        comments.forEach(comment => {
+            var elt = document.createElement('div');
+            elt.innerHTML = build_comment_html(comment);
+            if (!container.appendChild(elt)) {
+                console.error("ERROR: could not append element: ", elt);
+            }
+        })
+        container.style.display = 'block';
+        howto.href = issues_url;
+    }
+
+    find_comments_url()
+        .then(issue => fetch_comments(issue[0].html_url, issue[0].comments_url))
+        .then(([comments, issues_url]) => insert_comments(comments))
+        .catch(err => console.error("An error occurred while loading comments: ", err));
 }
