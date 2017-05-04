1391f20af332368fccf6a4834dbb7273495de786
Zephyr Pellerin <zv@nxvr.org>
2017-05-04 14:04:42 -0700

    Don't load page.js asynchronously

--- js/page.js
+++ js/page.js
@@ -5,16 +5,16 @@ import load_comments        from './comments';
     // Reposition footnotes onto right side
     if (window.innerWidth >= 1024) {
         window.addEventListener('load', function() {
-            let refs  = document.getElementsByClassName("footref")
-            let definitions = document.getElementsByClassName("footdef")
+            let refs  = document.getElementsByClassName("footref");
+            let definitions = document.getElementsByClassName("footdef");
             reposition_footnotes(refs, definitions);
         })
     }
 
     // Fetch & insert comments
     const CONTAINER_ELT = document.querySelector("div.comments-container");
-    let github_id       = CONTAINER_ELT.getAttribute("data-github-user")
-    let repo_issues_url = CONTAINER_ELT.getAttribute("data-issues-url")
-    let issue_id        = CONTAINER_ELT.getAttribute("data-issue-id")
-    load_comments(CONTAINER_ELT, github_id, repo_issues_url, issue_id)
-})()
+    let github_id       = CONTAINER_ELT.getAttribute("data-github-user");
+    let repo_issues_url = CONTAINER_ELT.getAttribute("data-issues-url");
+    let issue_id        = CONTAINER_ELT.getAttribute("data-issue-id");
+    load_comments(CONTAINER_ELT, github_id, repo_issues_url, issue_id);
+})();
